274d24cfd7aecfb2a2a9e4c84448236e39ca9bbd
Patrick Ellis <oneredoak@gmail.com>
2015-01-27 14:54:34 -0800

    Page Holders + Basic Routing Added

--- app/app.js
+++ app/app.js
@@ -1,41 +1,43 @@
 var express = require('express');
+var path = require('path');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// routing
+
 app.get('/', function(request, response) {
-    response.send({
-        test:'success'
-        page:'landingPage'
-    });
+    response.sendfile('public/index.html');
 });
 
 app.get('/form', function(request, response) {
-    response.send({
-        test:'success',
-        page:'form'
-    });
+    response.sendfile('public/form.html');
+
 });
 
 app.get('/thankyou', function(request, response) {
     response.send({
         test:'success',
-        page:'form'
+        page:'thankyou'
     });
 });
 
 app.get('/report', function(request, response) {
     response.send({
         test:'success',
-        page:'form'
+        page:'report'
     });
 });
 
 app.get('/about', function(request, response) {
     response.send({
         test:'success',
-        page:'form'
+        page:'about'
     });
 });
 
