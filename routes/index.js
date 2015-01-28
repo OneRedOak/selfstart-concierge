d4ce01bcbf170a3a8e56237d5da5ff553a48a8ce
Patrick Ellis <oneredoak@gmail.com>
2015-01-28 13:14:32 -0800

    Mongo + Monk Added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HI JUSTIN!' });
+});
+
+/* GET Hello World page. */
+router.get('/form', function(req, res) {
+    res.render('form', { title: 'Welcome to the form!' })
 });
 
 module.exports = router;
