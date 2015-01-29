6dd4acf4d1f3574d20545cebb40d840f370d0d4e
Patrick Ellis <oneredoak@gmail.com>
2015-01-29 12:54:29 -0800

    Addition of Mongoose + Mongo Wiring

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,15 @@ router.get('/form', function(req, res) {
     res.render('form', { title: 'Welcome to the form!' })
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
 module.exports = router;
