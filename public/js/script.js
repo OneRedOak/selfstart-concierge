55b7da2371b619d8e897251b36f0ab13c193ced1
Patrick Ellis <oneredoak@gmail.com>
2015-02-03 20:12:43 -0800

    Updated Front End Code + Assets

--- public/js/script.js
+++ public/js/script.js
@@ -54,9 +54,35 @@
     	},rhinoTime);
 
     	$('.learnmore').click(function(){
-    		$('#overallSection').ScrollTo();
-    		
+    		$('#nav').ScrollTo();
     	});
+
+    	$('#howTo').click(function(){
+    		$('#timelineSection').ScrollTo();
+    	});
+
+    	$('#aboutUs').click(function(){
+    		$('#nav').ScrollTo();
+    	});
+
+    	$('#getPresent').click(function(){
+    		$('#start').ScrollTo();
+    	});
+
+    	var my_posts = $("[rel=tooltip]");
+
+		var size = $(window).width();
+		for(i=0;i<my_posts.length;i++){
+			the_post = $(my_posts[i]);
+
+			if(the_post.hasClass('invert') && size >=767 ){
+				the_post.tooltip({ placement: 'left'});
+				the_post.css("cursor","pointer");
+			}else{
+				the_post.tooltip({ placement: 'rigth'});
+				the_post.css("cursor","pointer");
+			}
+		}
 	}
 
 	function typeWriter(text, n) {
