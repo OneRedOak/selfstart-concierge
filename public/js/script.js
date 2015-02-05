24b68d8c175389baa40a4c29440843aa76f79da8
Justin Ith <jith@u.washington.edu>
2015-02-04 17:51:02 -0800

    Last changes to surprise page before working on search page

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,29 @@
 (function() {
-	var rhinoTime = 2000;
+
 	window.setTimeout(function(){
-			// $('.learnmore').css('display', 'initial');
-    		// $('.learnmore').textillate();
-    		$('.topdescribe').fadeIn(1800, function(){
-    			setTimeout(function(){
-    				$('.topdescribe2').fadeIn(1800, function(){
-    					setTimeout(function(){
-		    				$('.learnmore').css('display', 'initial');
-		    				$('.learnmore').addClass('animated pulse infinite');
-    					},3500);
-    				});
-    			},0);
-    			
-    		});
-    	},3000);
+		$('.topdescribe').fadeIn(1800, function(){
+			setTimeout(function(){
+				$('.topdescribe2').fadeIn(1800, function(){
+					setTimeout(function(){
+	    				$('.learnmore').css('display', 'initial');
+	    				$('.learnmore').addClass('animated pulse infinite');
+					},3500);
+				});
+			},0);
+    	});
+    },3000);
+
     window.onload = function(){
     	setTimeout(function(){
 			$("#changing").typed({
-				strings: ["build a website", "learn node.js", "build an iOS app", "learn a new skill"],
+				strings: ["build a website", "learn node.js", "build an iOS app", "learn a new skill",""],
 				typeSpeed: 60
 			});
     	}, 800);
 
-    	$('#rhino').hover(
-    		function(){
-    			$('#rhino').attr('src','img/rhino-blue.png');
-    		}, function(){
-    			$('#rhino').attr('src','img/rhino.png');
-    		}
-		);
 
-    	setTimeout(function(){
-    		$('#rhino').css('right','-60');
-    		setTimeout(function(){
-    			$('#rhino').css('right','-50');
-    			setTimeout(function(){
-	    			$('#rhino').css('right','-40');
-	    			setTimeout(function(){
-		    			$('#rhino').css('right','-30');
-		    			setTimeout(function(){
-			    			$('#rhino').css('right','-20');
-			    			setTimeout(function(){
-				    			$('#rhino').css('right','-10');
-				    			setTimeout(function(){
-					    			$('#rhino').css('right','0');
-					    		},rhinoTime);
-				    		},rhinoTime);
-			    		},rhinoTime);
-		    		},rhinoTime);
-	    		},rhinoTime);
-    		},rhinoTime);
-    	},rhinoTime);
+    	setChevron();
+
 
     	$('.learnmore').click(function(){
     		$('#nav').ScrollTo();
@@ -68,6 +40,8 @@
     	$('#getPresent').click(function(){
     		$('#start').ScrollTo();
     	});
+
+    	// Timeline javascript
 
     	var my_posts = $("[rel=tooltip]");
 
@@ -93,6 +67,13 @@
 	      typeWriter(text, n)
 	    }, 100);
 	  }
+	}
+
+	function setChevron(){
+		var width = window.innerWidth / 3;
+		var border = width + "px solid transparent";
+		$('#triangle-down').css('border-left', border);
+		$('#triangle-down').css('border-right', border);
 	}
 
 })();
