ae886f9f00e29878b10556bd0374f20e16c141bc
Justin Ith <jith@u.washington.edu>
2015-02-07 23:45:49 -0800

    Added dynamic request generation based on JSON input

--- public/js/userDashboard.js
+++ public/js/userDashboard.js
@@ -8,6 +8,68 @@
 
     };
 
+    function populateRequests(data){
+        var obj = JSON.parse(data);
+        // alert(obj.requests.length);
+        for (var i = 0; i < obj.requests.length; i++) {
+            var targetRequest = obj.requests[i];
+            createRequest(targetRequest.title, targetRequest.status, targetRequest.link);
+        };
+    }
+
+    function createRequest(requestTitle, requestStatus, requestLink){
+        // Creates the over all request div
+        var container = document.createElement("div");
+        container.classList.add("request");
+
+        // Creates the title div + title heading
+        var title = document.createElement("div");
+        title.classList.add("title");
+        var titleHeading = document.createElement("h1");
+        titleHeading.innerHTML = requestTitle;
+        title.appendChild(titleHeading);
+       
+        container.appendChild(title);  // Puts title into request div
+
+        // Creates the attribute div
+        var attribute = document.createElement("div");
+        attribute.classList.add("attr");
+
+        // Creates status + current status
+        var status = document.createElement("div");
+        status.classList.add("status");
+        status.innerHTML = "Status :";
+        var currentStatus = document.createElement("span");
+        currentStatus.classList.add("currentStatus");
+        currentStatus.innerHTML = " Ready";
+        status.appendChild(currentStatus);
+
+        attribute.appendChild(status); // Puts status into attribute
+
+        // Creates the View button
+        var readyButton = document.createElement("div");
+        readyButton.classList.add("viewButton");
+        readyButton.innerHTML = "VIEW";
+
+        if (requestStatus) {
+            readyButton.classList.add("ready");
+
+            // Creates link to results
+            var link = document.createElement("a");
+            link.setAttribute("href", requestLink + "");
+            link.appendChild(readyButton);
+            attribute.appendChild(link); // Puts link + button in attribute
+        } else {
+            readyButton.classList.add("pending");
+            attribute.appendChild(readyButton); // Puts buttin in attribute
+        }
+
+        container.appendChild(attribute); // Puts attribute in results container
+
+        // Puts request in request list
+        document.getElementById("requestsList").appendChild(container);
+    }
+
     var userLogOut = function() {
         authTokenHandler().removeToken();
         $('#requestsList').hide();
