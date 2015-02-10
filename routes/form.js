2e6d2422e780315543d43982fffc3036faf70b54
Patrick Ellis <oneredoak@gmail.com>
2015-02-10 15:59:30 -0800

    Code Cleanup - LocalStrategy Refactored Into Services JS File

--- routes/form.js
+++ routes/form.js
@@ -1,90 +1,11 @@
 var express = require('express');
 var jwt = require('jwt-simple');
 var router = express.Router();
-var User = require('../models/user');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('../services/localStrategy.js')
 
-/* Strategy options for Passport authentication */
-var strategyOptions = {
-    usernameField: 'email',
-    passReqToCallback: true
-};
-
-/* Login strategy using Passport for verifying user login authenticity */
-var loginStrategy = new LocalStrategy(strategyOptions, function(req, email, password, done) {
-
-    var searchUser = {
-        email: email
-    };
-
-    User.findOne(searchUser, function(err, user) {
-        if (err) {
-            return done(err);
-        }
-
-        if(!user) {
-            return done(null, false, {message: 'Wrong email/password'});
-        }
-
-        user.comparePasswords(password, function(err, isMatch) {
-
-            if (err) {
-                return done(err);
-            }
-
-            if (!isMatch) {
-                return done(null, false, {message: 'Wrong email/password'});
-            }
-
-            return done(null, user);
-
-        });
-    });
-});
-
-/* Register strategy using Passport for verifying user registration authenticity */
-var registerStrategy = new LocalStrategy(strategyOptions, function(req, email, password, done) {
-
-    var searchUser = {
-        email: email
-    };
-
-    User.findOne(searchUser, function(err, user) {
-        if (err) {
-            return done(err);
-        }
-
-        if (user) {
-            return done(null, false, {message: 'Email already exists'});
-        }
-
-        var user = req.body;
-
-        var newUser = new User({
-            fullname: user.fullname,
-            email: user.email,
-            password: "selfstart",
-            query: user.query,
-            qlearn: user.qlearn,
-            qwhy: user.qwhy,
-            qbackground: user.qbackground,
-            qformat: user.qformat,
-            qbudget: user.qbudget,
-            qdate: new Date().getTime(),
-            rlink: null,
-            rstatus: null
-        });
-
-        newUser.save(function (err) {
-            done(null, newUser);
-        });
-
-    });
-});
-
-passport.use('local-register', registerStrategy);
-passport.use('local-login', loginStrategy);
+passport.use('local-register', LocalStrategy.register);
+passport.use('local-login', LocalStrategy.login);
 
 /* Test Post from User Sign Up Form */
 router.post('/register', passport.authenticate('local-register'), function (req, res) {
